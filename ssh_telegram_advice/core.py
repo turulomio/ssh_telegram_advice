5a8604528e71ec3a633a98d334927795aca14c4c
turulomio <turulomio@yahoo.es>
2022-04-25 17:06:10 +0200

    Works

--- ssh_telegram_advice/core.py
+++ ssh_telegram_advice/core.py
@@ -1,135 +1,48 @@
 from configparser import ConfigParser
-from datetime import datetime
-
+from datetime import datetime, timedelta
 from os import  makedirs
 from subprocess import check_output
- 
- 
- ## To get channel_id  https://api.telegram.org/bot<YourBOTToken>/getUpdates
-from telegram.ext import (Updater, CommandHandler)
+from time import sleep
 
-#from pyinotify import WatchManager, ProcessEvent, ALL_EVENTS, Notifier
-
-#class MyEventHandler(ProcessEvent):
-#    lastlog=datetime.now()
-#    fallidos=0
-#    validos=0
-#def process_IN_MODIFY(self, event):
-#
-#
 
 def start(update, context):
     ''' START '''
     context.bot.send_message(update.message.chat_id, "Bienvenido")
 
 def main():
-
-
+    lastlog=datetime.now()-timedelta(days=1)
+    
+    while True:
+        print(datetime.now())
+        cat = check_output("cat /var/log/messages| grep -i sshd", shell=True).decode("UTF-8")
+        
+        send=False
+        lines=cat.split('\n')
+        for line in lines:
+            d=str(datetime.now().year)+" " + line[:-len(line)+15]
+            if line.find('pam_unix(sshd:auth): authentication failure')!=-1 or line.find('pam_unix(sshd:session): session opened')!=-1:
+                try:
+                    nueva=datetime.strptime(d, '%Y %b %d %H:%M:%S')
+                except:
+                    print("error parsing date") 
+                if lastlog<nueva:
+                    lastlog=nueva
+                    send=True
+        if send==True:
+            send_message_with_requests(cat[-1000:])
+        sleep(10)
+            
+def send_message_with_requests(message):
+    import requests
     makedirs("/etc/ssh_telegram_advice", exist_ok=True)
     config = ConfigParser()
     config.read("/etc/ssh_telegram_advice/ssh_telegram_advice")
-    api_id = config["Telegram"]["api_id"]
-    api_hash = config["Telegram"]["api_hash"]
     token= config["Telegram"]["token"]
-    phone= config["Telegram"]["phone"]
-    username= config["Telegram"]["username"]
-    print(api_id, api_hash, phone, username)
-    
-#    send_message("HOLA")
-    
-#    
-#    
-#    # event handler
-#    eh = MyEventHandler()
-#
-#    # notifier
-#    notifier = Notifier(wm, eh)
-#    notifier.loop()
+    chat_id= config["Telegram"]["chat_id"]
+    d={"chat_id": chat_id,  "text": message[-00:]}
+    url = f"https://api.telegram.org/bot{token}/sendMessage"
+    r=requests.post(url, d)
+    sleep(0.5)
+    print("Message sent",  url, r, d)
 
-
-    updater=Updater(token, use_context=True)
-    dp=updater.dispatcher
-
-    # Eventos que activarán nuestro bot.
-    dp.add_handler(CommandHandler('start',	start))
-
-    # Comienza el bot
-    updater.start_polling()
-    # Lo deja a la escucha. Evita que se detenga.
-    import request
-
-    url = f"https://api.telegram.org/bot{token}/sendMessage?chat_id=@telechanneltesting&text=message"
-    requests.post(url)
-    
-    
-    
-    return
-    lastlog=datetime.now()
-    fallidos=0
-    validos=0
-    
-    cat = check_output("cat /var/log/messages| grep -i sshd", shell=True)
-    for line in cat.split(b'\n'):
-        line=line.decode("UTF-8")
-        print (line[:-1])
-        d=str(datetime.now().year)+" " + line[:-len(line)+15]
-        if line.find('pam_unix(sshd:auth): authentication failure')!=-1:
-            try:
-                nueva=datetime.strptime(d, '%Y %b %d %H:%M:%S')
-            except:
-                print("error parsing date")
-            if lastlog<nueva:
-                fallidos=fallidos+1
-                send_message("espeak -v es 'Conexión remota fallida número {0}' >& /dev/null".format(fallidos))
-            lastlog=nueva
-        elif line.find('pam_unix(sshd:session): session opened for user ')!=-1:
-            try:
-                nueva=datetime.strptime(d, '%Y %b %d %H:%M:%S')
-            except:
-                print("error parsing date")
-            if lastlog<nueva:
-                validos=validos+1
-                send_message("espeak -v es 'Conexión remota aceptada número {0}' >& /dev/null".format(validos))
-            lastlog=nueva
-    
-def send_message_with_telethon(message):
-    from telethon import TelegramClient
-    config = ConfigParser()
-    config.read("/etc/ssh_telegram_advice/ssh_telegram_advice")
-    api_id = config["Telegram"]["api_id"]
-    api_hash = config["Telegram"]["api_hash"]
-    phone= config["Telegram"]["phone"]
-    username= config["Telegram"]["username"]
-    print(api_id, api_hash, phone, username)
-    client = TelegramClient(username, api_id, api_hash)
-  
-    # connecting and building the session
-    client.connect()
-     
-    # in case of script ran first time it will
-    # ask either to input token or otp sent to
-    # number or sent or your telegram id
-    if not client.is_user_authorized():
-      
-        client.send_code_request(phone)
-         
-        # signing in the client
-        client.sign_in(phone, input('Enter the code: '))
-      
-      
-    try:
-        # receiver user_id and access_hash, use
-        # my user_id and access_hash for reference
-#        receiver = InputPeerUser(
-     
-        # sending message using telegram client
-        client.send_message(username, message, parse_mode='html')
-    except Exception as e:
-         
-        # there may be many error coming in while like peer
-        # error, wrong access_hash, flood_error, etc
-        print(e);
-     
-    # disconnecting the telegram session
-    client.disconnect()
     
