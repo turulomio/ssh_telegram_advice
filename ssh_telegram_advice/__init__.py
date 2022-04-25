e2a1cd4acbc5c50a084e57f899d6ad54755cc4d4
turulomio <turulomio@yahoo.es>
2022-04-25 14:25:17 +0200

    Trying

--- ssh_telegram_advice/__init__.py
+++ ssh_telegram_advice/__init__.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 __version__ = '0.1.0'
 __versiondatetime__=datetime(2022, 4, 25, 8, 0)
 __versiondate__=__versiondatetime__.date()
--- ssh_telegram_advice/core.py
+++ ssh_telegram_advice/core.py
@@ -1,52 +1,135 @@
-#!/usr/bin/python3
-# Created by Pathogen @ www.hakhub.tk
+from configparser import ConfigParser
+from datetime import datetime
 
-import datetime
-import subprocess 
-import re, os
-import pyinotify
+from os import  makedirs
+from subprocess import check_output
+ 
+ 
+ ## To get channel_id  https://api.telegram.org/bot<YourBOTToken>/getUpdates
+from telegram.ext import (Updater, CommandHandler)
 
-class MyEventHandler(pyinotify.ProcessEvent):
-	lastlog=datetime.datetime.now()
-	fallidos=0
-	validos=0
-	def process_IN_MODIFY(self, event):
-		cat = subprocess.check_output("cat /var/log/messages| grep -i sshd",shell=True)
-		for line in cat.split(b'\n'):
-			d=str(datetime.datetime.now().year)+" " + line[:-len(line)+15].decode('UTF-8')
-			if line.find(b'pam_unix(sshd:auth): authentication failure')!=-1:
-				try:
-					nueva=datetime.datetime.strptime(d, '%Y %b %d %H:%M:%S')
-				except:
-					print("error parsing date")
-				if self.lastlog<nueva:
-					self.fallidos=self.fallidos+1
-					os.system("espeak -v es 'Conexión remota fallida número {0}' >& /dev/null".format(self.fallidos))
-					self.lastlog=nueva
-					print (line[:-1])
-			elif line.find(b'pam_unix(sshd:session): session opened for user ')!=-1:
-				try:
-					nueva=datetime.datetime.strptime(d, '%Y %b %d %H:%M:%S')
-				except:
-					print("error parsing date")
-				if self.lastlog<nueva:
-					self.validos=self.validos+1
-					os.system("espeak -v es 'Conexión remota aceptada número {0}' >& /dev/null".format(self.validos))
-					self.lastlog=nueva
-					print (line[:-1])
+#from pyinotify import WatchManager, ProcessEvent, ALL_EVENTS, Notifier
+
+#class MyEventHandler(ProcessEvent):
+#    lastlog=datetime.now()
+#    fallidos=0
+#    validos=0
+#def process_IN_MODIFY(self, event):
+#
+#
+
+def start(update, context):
+    ''' START '''
+    context.bot.send_message(update.message.chat_id, "Bienvenido")
 
 def main():
 
-	# watch manager
-	wm = pyinotify.WatchManager()
-	wm.add_watch('/var/log/messages', pyinotify.ALL_EVENTS, rec=True)
 
-	# event handler
-	eh = MyEventHandler()
+    makedirs("/etc/ssh_telegram_advice", exist_ok=True)
+    config = ConfigParser()
+    config.read("/etc/ssh_telegram_advice/ssh_telegram_advice")
+    api_id = config["Telegram"]["api_id"]
+    api_hash = config["Telegram"]["api_hash"]
+    token= config["Telegram"]["token"]
+    phone= config["Telegram"]["phone"]
+    username= config["Telegram"]["username"]
+    print(api_id, api_hash, phone, username)
+    
+#    send_message("HOLA")
+    
+#    
+#    
+#    # event handler
+#    eh = MyEventHandler()
+#
+#    # notifier
+#    notifier = Notifier(wm, eh)
+#    notifier.loop()
 
-	# notifier
-	notifier = pyinotify.Notifier(wm, eh)
-	notifier.loop()
 
+    updater=Updater(token, use_context=True)
+    dp=updater.dispatcher
 
-main()
+    # Eventos que activarán nuestro bot.
+    dp.add_handler(CommandHandler('start',	start))
+
+    # Comienza el bot
+    updater.start_polling()
+    # Lo deja a la escucha. Evita que se detenga.
+    import request
+
+    url = f"https://api.telegram.org/bot{token}/sendMessage?chat_id=@telechanneltesting&text=message"
+    requests.post(url)
+    
+    
+    
+    return
+    lastlog=datetime.now()
+    fallidos=0
+    validos=0
+    
+    cat = check_output("cat /var/log/messages| grep -i sshd", shell=True)
+    for line in cat.split(b'\n'):
+        line=line.decode("UTF-8")
+        print (line[:-1])
+        d=str(datetime.now().year)+" " + line[:-len(line)+15]
+        if line.find('pam_unix(sshd:auth): authentication failure')!=-1:
+            try:
+                nueva=datetime.strptime(d, '%Y %b %d %H:%M:%S')
+            except:
+                print("error parsing date")
+            if lastlog<nueva:
+                fallidos=fallidos+1
+                send_message("espeak -v es 'Conexión remota fallida número {0}' >& /dev/null".format(fallidos))
+            lastlog=nueva
+        elif line.find('pam_unix(sshd:session): session opened for user ')!=-1:
+            try:
+                nueva=datetime.strptime(d, '%Y %b %d %H:%M:%S')
+            except:
+                print("error parsing date")
+            if lastlog<nueva:
+                validos=validos+1
+                send_message("espeak -v es 'Conexión remota aceptada número {0}' >& /dev/null".format(validos))
+            lastlog=nueva
+    
+def send_message_with_telethon(message):
+    from telethon import TelegramClient
+    config = ConfigParser()
+    config.read("/etc/ssh_telegram_advice/ssh_telegram_advice")
+    api_id = config["Telegram"]["api_id"]
+    api_hash = config["Telegram"]["api_hash"]
+    phone= config["Telegram"]["phone"]
+    username= config["Telegram"]["username"]
+    print(api_id, api_hash, phone, username)
+    client = TelegramClient(username, api_id, api_hash)
+  
+    # connecting and building the session
+    client.connect()
+     
+    # in case of script ran first time it will
+    # ask either to input token or otp sent to
+    # number or sent or your telegram id
+    if not client.is_user_authorized():
+      
+        client.send_code_request(phone)
+         
+        # signing in the client
+        client.sign_in(phone, input('Enter the code: '))
+      
+      
+    try:
+        # receiver user_id and access_hash, use
+        # my user_id and access_hash for reference
+#        receiver = InputPeerUser(
+     
+        # sending message using telegram client
+        client.send_message(username, message, parse_mode='html')
+    except Exception as e:
+         
+        # there may be many error coming in while like peer
+        # error, wrong access_hash, flood_error, etc
+        print(e);
+     
+    # disconnecting the telegram session
+    client.disconnect()
+    
